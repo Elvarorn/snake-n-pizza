595af02fb8c130d6207f6fc516d28b867f8f2c41
Joey <johanneinar95@gmail.com>
2018-03-07 19:51:29 +0000

    added NavBar render

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,11 @@ import thunk from 'redux-thunk';
 import reducers from './reducers/reducers';
 import Menu from './components/Menu/Menu';
 import Offers from './components/Offers/Offers';
+import NavigationBar from './components/NavigationBar/NavigationBar';
 
 const App = () => {
   return (<div className="menu-offers">
+            <NavigationBar logoImageUrl="https://www1-lw.xda-cdn.com/files/2012/11/pizza-snake.jpg" />
             <Menu/>
             <p> offers :</p>
             <Offers/>
