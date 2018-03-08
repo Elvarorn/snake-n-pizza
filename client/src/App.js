645b2eaa36de5df0b39a6bfa79d54f983b8ba5af
Elvarorn <elvar16@ru.is>
2018-03-08 23:18:36 +0000

    Final push

--- client/src/App.js
+++ client/src/App.js
@@ -10,21 +10,30 @@ import Cart from './components/Cart/Cart';
 import PizzaDetail from './components/PizzaDetail/PizzaDetail';
 import AboutUs from './components/AboutUs/AboutUs';
 import NavigationBar from './components/NavigationBar/NavigationBar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 const App = () => {
+  if(window.location.pathname == '/' || window.location.pathname == '/Offers' || window.location.pathname == '/AboutUs' || window.location.pathname == '/Cart' || window.location.pathname == '/pizza' ) {
+  return (
+      <div className="menu-offers">
+          <NavigationBar logoImageUrl="https://image.ibb.co/jxR33S/logosup.png" />
+          <Switch>
+              <Route exact path="/" component={ Menu } />
+              <Route path="/Offers" component={ Offers } />
+              <Route path="/AboutUs" component={ AboutUs } />
+              <Route path="/Cart" component={ Cart } />
+              <Route path="/pizza" component= { PizzaDetail } />
+          </ Switch>
+      </div>
+      );
+  }
+  else {
     return (
-        <div className="menu-offers">
-            <NavigationBar logoImageUrl="https://image.ibb.co/jxR33S/logosup.png" />
-            <Switch>
-                <Route exact path="/" component={ Menu } />
-                <Route path="/Offers" component={ Offers } />
-                <Route path="/AboutUs" component={ AboutUs } />
-                <Route path="/Cart" component={ Cart } />
-                <Route path="/pizza" component= { PizzaDetail } />
-            </ Switch>
-        </div>
+      <div>
+        <Redirect to="/" />
+      </ div>
     );
+  }
 };
 
 ReactDOM.render(<Provider store={createStore(reducers, applyMiddleware(thunk))}><Router><App /></ Router></Provider>, document.getElementById('app'));
--- client/src/components/Pizza/Pizza.js
+++ client/src/components/Pizza/Pizza.js
@@ -8,10 +8,9 @@ var cartItemArr = [];
 var pizzaToDetail;
 const Pizza = ({ pizza }) => {
 
-<<<<<<< HEAD
     var addToCartItemArr = (pizza) =>{
         cartItemArr.push(pizza);
-        console.log(cartItemArr);
+        alert('The pizza has been added to the cart, please proceed to the cart to finish your purchase!');
         localStorage.setItem('items-in-cart', JSON.stringify(cartItemArr));
     }
 
@@ -31,9 +30,9 @@ const Pizza = ({ pizza }) => {
                 </div>
                 <div className="pizza-name">{name}</div>
 
-                <button className="add-pizza-to-cart-button" onClick={() => addToCartItemArr(pizza)}>Add to Cart</button>
-                <Link to={`pizza/${pizza.id}`}>
-                    <button className="pizza-to-detail-button" onClick={() => pizzaDetail(pizza)}>Details about the pizza!</button>
+                <button className="menu-button button" onClick={() => addToCartItemArr(pizza)}>Add to Cart</button>
+                <Link to={'/pizza'}>
+                    <button className="menu-button button" onClick={() => pizzaDetail(pizza)}>Details about the pizza!</button>
                 </Link>
             </div>
         );
@@ -49,48 +48,6 @@ const Pizza = ({ pizza }) => {
             </div>
         );
     }
-=======
-var addToCartItemArr = (pizza) =>{
-  cartItemArr.push(pizza);
-  alert('Item added to cart, please proceed to the cart to finish your purchase')
-  localStorage.setItem('items-in-cart', JSON.stringify(cartItemArr));
-}
-
-var pizzaDetail = (pizza) => {
-  pizzaToDetail = pizza;
-  localStorage.setItem('pizza-to-detail', JSON.stringify(pizzaToDetail));
-}
-
-  const {name, description, price, image } = pizza;
-
-  if(window.location.pathname == '/') {
-    return (
-      <div className="pizza-wrapper">
-
-          <div className="pizza-image">
-              <img src={image} alt=""/>
-          </div>
-          <div className="pizza-name">{name}</div>
-          <button className="menu-button button" onClick={() => addToCartItemArr(pizza)}>Add to Cart</button>
-          <Link to={`pizza/${pizza.id}`}>
-          <button className="menu-button button" onClick={() => pizzaDetail(pizza)}>Details about the pizza!</button>
-          </Link>
-      </div>
-    );
-  } else {
-    return (
-      <div className="pizza-wrapper">
-          <div className="pizza-image">
-              <img src={image} alt=""/>
-          </div>
-          <div className="pizza-name">{name}</div>
-          <div className="pizza-description">{description}</div>
-          <div className="pizza-price">{price}</div>
-      </div>
-    );
-  }
->>>>>>> 478536102edace4e4c67298a076a4eedf8f76777
-
 };
 
 Pizza.PropTypes = {
